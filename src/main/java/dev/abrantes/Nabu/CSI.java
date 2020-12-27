c89e75d19666bdebe6ab660d55e593cab4ddd24d
raphaelabrantes <antenabr2@gmail.com>
2020-12-26 23:23:56 -0300

    Class for ASCII Control Sequence Introducer commands

---
+++ src/main/java/dev/abrantes/Nabu/CSI.java
@@ -0,0 +1,12 @@
+package dev.abrantes.Nabu;
+
+public class CSI {
+    static private final String c = "\033[";
+
+    public static String eraseLineMoveUp(int n) {
+        String cleanUp = c + "2K" + c + "1F";
+        StringBuilder returnToStart = new StringBuilder(cleanUp);
+        returnToStart.append(cleanUp.repeat(Math.max(0, n - 1)));
+        return returnToStart.toString();
+    }
+}
