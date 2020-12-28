b3752b315af4e1b61db4c8ea8b8deec7f550681a
raphaelabrantes <antenabr2@gmail.com>
2020-12-27 23:25:22 -0300

    Major rework on how the CPU metric works. Don't drink and program
    Added:
    generateCores(), adds the created cores to the Core arraylist
    createCore(), creates the core with info
    updateCore() updates specific core
    refreshCores() calls updateCore for each core
    calculateUsageTotal() calculate the total usage of one core
    
    Modified:
    getCoreTemps() -> getCpuTemps()
    ArrayList<String> cpus -> ArrayList<Core> cores
    tempFile1 -> tempFileTdie
    tempFile2 -> tempFileCTL
    
    Removed:
    getCoreUsage()

--- src/main/java/dev/abrantes/Nabu/metrics/CPU.java
+++ src/main/java/dev/abrantes/Nabu/metrics/CPU.java
@@ -7,62 +7,105 @@ import java.util.Scanner;
 public class CPU {
     private final String[] command = new String[]{"grep", "cpu", "/proc/stat"};
     private final String tempPath = "/sys/class/hwmon/hwmon0/temp%d_input";
-    private final File tempFile1 = new File(String.format(tempPath, 1));
-    private final File tempFile2 = new File(String.format(tempPath, 2));
-
-    private ArrayList<String> cpus;
-    private ArrayList<Double> cpuTemp;
+    private final File tempFileTdie = new File(String.format(tempPath, 1));
+    private final File tempFileCTL = new File(String.format(tempPath, 2));
+    private final int IDLE_INDEX = 4;
+    private final int PROCESSN = 7;
+    private final int NAME_INDEX = 0;
+    private final ArrayList<Core> cores = new ArrayList<>();
+    private final ArrayList<Double> cpuTemp = new ArrayList<>();
 
     public CPU() {
+        generateCores();
         get_metrics();
     }
 
     public void get_metrics() {
+        double tempDie = readTemp(tempFileTdie);
+        double tempCTL = readTemp(tempFileCTL);
+        cpuTemp.clear();
+        cpuTemp.add(tempDie);
+        cpuTemp.add(tempCTL);
+        refreshCores();
+    }
+
+    public void generateCores(){
         try {
             Process proc = new ProcessBuilder(command).start();
             BufferedReader reader =
                     new BufferedReader(new InputStreamReader(proc.getInputStream()));
 
+            String allCpu = reader.readLine();
             String line = "";
-            cpus = new ArrayList<String>();
             while ((line = reader.readLine()) != null) {
-                cpus.add(line);
+                Core newCore = createCore(line, false );
+                cores.add(newCore);
             }
+            reader.close();
+            Core totalCore = createCore(allCpu, true);
+            cores.add(totalCore);
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-        double temp1 = readTemp(tempFile1);
-        double temp2 = readTemp(tempFile2);
-        cpuTemp = new ArrayList<Double>();
-        cpuTemp.add(temp1);
-        cpuTemp.add(temp2);
+    }
+
+    private void refreshCores(){
+        try {
+            Process proc = new ProcessBuilder(command).start();
+            BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(proc.getInputStream()));
+
+            String allCpu = reader.readLine();
+            updateCore(allCpu, IDLE_INDEX + 1, 2, getNCores());
+            for(int core = 0; core < getNCores(); core++){
+                updateCore(reader.readLine(), IDLE_INDEX, 1, core);
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void updateCore(String line, int idleIndex, int startIndex, int core){
+        String [] coreInfo = line.split(" ");
+        long idle = parser(idleIndex, coreInfo);
+        long total = calculateUsageTotal(startIndex, coreInfo);
+        cores.get(core).refresh(total, idle);
+    }
+
+    public ArrayList<Core> getCores() {
+        return cores;
     }
 
     public int getNCores() {
-        return cpus.size() - 1;
+        return cores.size() - 1;
     }
 
-    public double getCoreUsage(int core) {
-        int search = core != 13 ? core + 1 : 0;
-        int index = core != 13 ? 1 : 2;
-        String[] coreInfo = cpus.get(search).split(" ");
-        System.out.println(coreInfo[0]);
-        long user = parser(index++, coreInfo);
-        long nice = parser(index++, coreInfo);
-        long system = parser(index++, coreInfo);
-        long idle = parser(index++, coreInfo);
-        long iowait = parser(index++, coreInfo);
-        long irq = parser(index++, coreInfo);
-        long softirq = parser(index, coreInfo);
-        long total = user + nice + idle + system + iowait + irq + softirq;
-        return (total - idle) * 100.0 / total;
+    private Core createCore(String line, boolean isAll){
+        String[] coreInfo = line.split(" ");
+        int plus = isAll ? 1:0;
+        long total = calculateUsageTotal(1+plus, coreInfo);
+        String name = coreInfo[NAME_INDEX];
+        long idle = parser(IDLE_INDEX + plus, coreInfo);
+        return new Core(name, total, idle);
+    }
+
+    private long calculateUsageTotal(int index, String [] coreInfo) {
+        long total = 0;
+        for(int count = 0; count < PROCESSN; count++, index++){
+            total += parser(index, coreInfo);
+        }
+        return total;
     }
 
     private long parser(int index, String[] coreinfo){
         return Long.parseLong(coreinfo[index]);
     }
 
-    public ArrayList<Double> getCoreTemps() {
+    public ArrayList<Double> getCpuTemps() {
         return cpuTemp;
     }
 
@@ -80,3 +123,4 @@ public class CPU {
 
     }
 }
+
