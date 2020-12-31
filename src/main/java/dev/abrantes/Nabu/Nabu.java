15d583c645d9d214fe6d90815a6f03e616f71343
raphaelabrantes <antenabr2@gmail.com>
2020-12-31 00:27:56 -0300

    Added gpu object, it is still a beta version

--- src/main/java/dev/abrantes/Nabu/Nabu.java
+++ src/main/java/dev/abrantes/Nabu/Nabu.java
@@ -1,21 +1,26 @@
 package dev.abrantes.Nabu;
 
 import dev.abrantes.Nabu.APIs.WeatherApi;
-import dev.abrantes.Nabu.metrics.CPU;
-import dev.abrantes.Nabu.metrics.Core;
+import dev.abrantes.Nabu.metrics.CPU.CPU;
+import dev.abrantes.Nabu.metrics.CPU.Core;
+import dev.abrantes.Nabu.metrics.GPU.GPU;
+import jdk.jshell.spi.ExecutionControl;
 
 import java.util.ArrayList;
 import java.util.Locale;
+
+import static jdk.jshell.spi.ExecutionControl.*;
 
 public class Nabu {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         WeatherApi weather = new WeatherApi("Santos");
         CPU cpu = new CPU();
+        GPU gpu = new GPU("amd");
         System.out.printf("Temperature in %s: %.2f°C%s%n%n",
                 weather.getCity(),
                 weather.getTemperature(),
-                "\n".repeat(cpu.getNCores()+2));
+                "\n".repeat(cpu.getNCores()+4));
 
         ArrayList<Core> cores = cpu.getCores();
         ArrayList<Double> cpuTemps = cpu.getCpuTemps();
@@ -23,13 +28,14 @@ public class Nabu {
             try {
                 Thread.sleep(500);
                 cpu.get_metrics();
-                System.out.print(CSI.eraseLineMoveUp(cpu.getNCores() + 3) + CSI.eraseLine());
+                System.out.print(CSI.eraseLineMoveUp(cpu.getNCores() + 5) + CSI.eraseLine());
+                System.out.printf("Temperature GPU %.2f°C%nUsage GPU: %d%s%n", gpu.getGpuTemp(), gpu.getUsage(), "%");
                 System.out.printf("Temperature Tdie (REAL) of CPU: %.2f°C\nTemperature CTL of CPU: %.2f°C%n",
                         cpuTemps.get(0), cpuTemps.get(1));
                 for (Core core: cores) {
                     System.out.printf("%s: %.2f%s%n", core.getCoreName(), core.getUsage(), "%");
                 }
-            } catch (InterruptedException e) {
+            } catch (InterruptedException | NotImplementedException e) {
                 e.printStackTrace();
                 System.exit(0);
             }
